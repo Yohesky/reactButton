858ffb4c00d67337fa9fe7914b12f912efe39c2d
Yohesky <yoheskyjpp@gmail.com>
2020-09-06 01:04:47 -0400

    docs add

---
+++ docs/precache-manifest.9932d4bdf167d5cea976b5671a9190e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66000e24343c6d2462c7322671836a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4ababfef7f108b0872a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7f2581935b4dd20da700",
+    "url": "/static/js/2.bff6217e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bff6217e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ababfef7f108b0872a",
+    "url": "/static/js/main.71e3baa3.chunk.js"
+  },
+  {
+    "revision": "67456f03d76baaec320a",
+    "url": "/static/js/runtime-main.55b19956.js"
+  }
+]);
